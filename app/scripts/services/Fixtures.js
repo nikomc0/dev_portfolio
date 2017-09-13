78cae01eae0ba1ad0c4988c47a492550a2c017fa
Dominick <dominick@theosfp.com>
2017-09-12 21:48:12 -0700

    Updated projects page

--- app/scripts/services/Fixtures.js
+++ app/scripts/services/Fixtures.js
@@ -4,62 +4,24 @@
     var Fixtures = {};
 
     var projects = {
-      projectBlocJams: {
-        title: "Bloc Jams",
-        developer: "Dominick Del Bosque",
-        year: "2017",
-        type: "Front End",
-        screenshotUrl: '/assets/images/SoundCloud.jpg',
-        description: "Lorem Ipsum dolor..."
-      },
-
       projectSquatter: {
         title: "Squatter",
         developer: "Dominick Del Bosque",
         year: "2017",
-        type: "Front / Back End",
-        screenshotUrl: '../../assets/images/background-dark.jpg',
-        description: "Lorem Ipsum dolor..."
-      },
-
-      projectBloccipedia: {
-        title: "Bloccipedia",
-        developer: "Dominick Del Bosque",
-        year: "2017",
-        type: "Front / Back End",
-        screenshotUrl: '../../assets/images/background-dark.jpg',
-        description: "Lorem Ipsum dolor..."
-      },
-
-      projectBabyDrop: {
-        title: "BabyDrop",
-        developer: "Dominick Del Bosque",
-        year: "2017",
         type: "Front End / Back End",
-        screenshotUrl: '/assets/images/SoundCloud.jpg',
+        screenshotUrl: '../../assets/images/squat.png',
         description: "Lorem Ipsum dolor..."
       },
 
-      projectThisDamnWorld: {
-        title: "ThisDamnWorld",
+      projectSandbox: {
+        title: "Sandbox Suites",
         developer: "Dominick Del Bosque",
         year: "2017",
         type: "Front End",
-        screenshotUrl: '/assets/images/SoundCloud.jpg',
+        screenshotUrl: '../../assets/images/sandbox.png',
         description: "Lorem Ipsum dolor..."
       },
-
-      projectBlocChat: {
-        title: "BlocChat",
-        developer: "Dominick Del Bosque",
-        year: "2017",
-        type: "Front / Back End",
-        screenshotUrl: '../../assets/images/background-dark.jpg',
-        description: "Lorem Ipsum dolor..."
-      }
     };
-
-
 
     Fixtures.getProjects = function () {
       return projectBlocJams;
@@ -69,7 +31,6 @@
       this.projects = [];
 
       for (var projectName in projects) {
-        console.log(projects[projectName]);
         this.projects.push(angular.copy(projects[projectName]));
       }
 
